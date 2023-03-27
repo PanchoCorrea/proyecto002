6ec262bd6b587b1e94d3b8028d4cf5e7554a468d
PanchoCorrea <correafrancisco2003@gmail.com>
2023-03-27 14:23:05 -0300

    se agrega el proyecto

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function mostrarTitulo(tit) {
+  return (<h1>
+    {tit}
+  </h1>);
+}
+
+function App() {
+  return (
+    <div>
+      {mostrarTitulo('Hola Mundo')}
+      {mostrarTitulo('Fin')}
+    </div>
+  );
+}
+
+export default App;
